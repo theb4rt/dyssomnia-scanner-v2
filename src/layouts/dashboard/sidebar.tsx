eb5ee5f9a2925aa309e416a4e8b42355d61efb09
B4rt <b4rt@linux.com>
2026-02-23 17:54:33 -0500

    feat: deploy version 2.1

--- src/layouts/dashboard/sidebar.tsx
+++ src/layouts/dashboard/sidebar.tsx
@@ -25,7 +25,7 @@ export function AppSidebar() {
               Dyssomnia
             </span>
             <span className="font-mono text-[10px] text-muted-foreground">
-              Scanner v2.0
+              Scanner v2.1
             </span>
           </div>
         </div>
